e0543f5226c5070404f08b17b37449de48b0086d
Jerome <87921319+Jebep33@users.noreply.github.com>
2025-03-29 07:27:28 +0800

    Added button values, and operations

--- calculate.js
+++ calculate.js
@@ -1,4 +1,33 @@
 var num1, num2, operation;
+let buttons = document.querySelectorAll("button");
+let input = document.querySelector("input");
+let operationClicked = false;
+
+buttons.forEach(button => {
+    button.addEventListener("click",event => {
+        switch(event.target.id){
+            case ('one'): appendString('1'); break;
+            case ('two'): appendString('2'); break;
+            case ('three'): appendString('3'); break;
+            case ('four'): appendString('4'); break;
+            case ('five'): appendString('5'); break;
+            case ('six'): appendString('6'); break;
+            case ('seven'): appendString('7'); break;
+            case ('eight'): appendString('8'); break;
+            case ('nine'): appendString('9'); break;
+            case ('zero'): appendString('0'); break;
+            case ('dot'): appendString('.'); break;
+            case ('plus'): 
+            case ('times'):
+            case ('divide'):
+            case ('minus'):
+                storeValue(input.value);
+                console.log(`${num1},${operationClicked}`);
+                break;
+            default: alert("Invalid"); break;
+        }
+    });
+});
 
 function add(a,b){
     return a+b;
@@ -6,7 +35,7 @@ function add(a,b){
 
 function subtract(a,b){
     return a-b;
-}_
+}
 
 function multiply(a,b){
     return a*b;
@@ -24,4 +53,14 @@ function operate(num1,operation,num2){
         case '/': divide(num1,num2); break;
         default: alert("Invalid!");
     }
+}
+
+function appendString(stringToAppend){
+    input.value = input.value + stringToAppend;
+}
+
+function storeValue(value){
+    num1 = parseInt(value);
+    input.value = "";
+    operationClicked = true;
 }
